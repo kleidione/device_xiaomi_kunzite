304c2001e8486c96fd68021b060bbc5d2b22e7df
Arian <arian.kulmer@web.de>
2026-03-10 11:55:37 -0300

    kunzite: Allow surfaceflinger to use the big cluster
    task_profiles.json assigns surfaceflinger to the system-background
    cgroup which is limited to the little cluster (cpus 0-3). Scrolling
    through the recent apps menu with blur enabled seems to be too heavy
    for the little cluster, leading to significant stuttering. Observing
    the perfetto trace during this interaction reveals that the little
    cluster is very busy and exceeds many expected timelines, while the big
    and prime clusters are idle. To balance the cpu usage among the
    available resources, create a new cgroup for surfaceflinger which can
    use the cpus 4-7. Copy the permissions setup from init.rc for
    system-background.
    
    Test:
    Observe the cpu usage in perfetto trace and see surfaceflinger is also
    assigned to the big cluster and the expected timelines match the actual
    timeline:
    * adb shell perfetto \
      -o /data/misc/perfetto-traces/trace.perfetto-trace \
      -t 5s sched freq idle am wm gfx view
    * while it is running, open the recent apps menu and scroll
    * adb pull /data/misc/perfetto-traces/trace.perfetto-trace
    * analyze on https://ui.perfetto.dev
    
    Change-Id: I32b1e488678783691a3dee17d75ae284b3cc4c82

--- device.mk
+++ device.mk
@@ -258,6 +258,7 @@ PRODUCT_PACKAGES += \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/power/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Properties
