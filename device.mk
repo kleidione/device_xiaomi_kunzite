ceca09fab97680fef6d4519cea34fb3df7683a62
heybyben <heybyben@gmail.com>
2026-03-16 12:18:50 -0300

    kunzite: Use snapdragon_color_libs_config.xml from source
    Change-Id: I43e1e8c25d7f56daf7d04fdc9a20c72d7c293612

--- device.mk
+++ device.mk
@@ -128,6 +128,9 @@ $(foreach display_id, 4630946494833437058 4630946610401717122 463094683242527040
     ))
 
 PRODUCT_COPY_FILES += \
+    hardware/qcom-caf/sm8450-6.6/display/hal/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
+
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
 # DRM
